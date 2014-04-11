0605ae919960e9de9edba009da3f799f526708fc
luca <luca@ubuntu.ubuntu-domain>
2014-04-11 14:51:38 +0200

    prova 01

---
+++ src/kazaa.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="luca"
+__date__ ="$11-apr-2014 14.47.37$"
+
+if __name__ == "__main__":
+    print "Hello World223"
