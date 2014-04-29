95df04ee6dad6a622d473d2288b68b2781c478a8
luca <luca@ubuntu.ubuntu-domain>
2014-04-29 16:59:55 +0200

    prova 03

--- src/Server.py
+++ src/Server.py
@@ -312,7 +312,7 @@ class Server:
             i = i + 1  
         
         print("\t->Attesa dei risultati...")
-        time.sleep(Util.Util.SLEEPTIME)
+        time.sleep(Util.SLEEPTIME)
         
         conn_db = Connessione.Connessione()
         searchResults = SearchResultService.SearchResultService.getSearchResults(conn_db.crea_cursore(), pkt.idpacket, searchString)
